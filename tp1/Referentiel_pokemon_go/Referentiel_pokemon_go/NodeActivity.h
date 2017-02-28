c3bc874afb9e9cf8544d7db943014eb89b1ad6d3
locora <louis.cormier@polymtl.ca>
2017-02-28 18:37:28 -0500

    Ajout des entêtes de NodeActivity

--- tp1/Referentiel_pokemon_go/Referentiel_pokemon_go/NodeActivity.h
+++ tp1/Referentiel_pokemon_go/Referentiel_pokemon_go/NodeActivity.h
@@ -6,13 +6,29 @@
 class NodeActivity
 {
 public:
+#pragma region ConstructorDestructor
+	//! A default constructor
 	NodeActivity();
+	//! A destructor
 	~NodeActivity();
+#pragma endregion ConstructorDestructor
 
+	//! A function that set to Inactive a node
+	/*!
+	* \param node : The node to make inactive
+	*/
 	void setNodeToInactive(std::shared_ptr<AbstractNode> node);
 
+	//! A function that remove a pair from the inactive nodes
+	/*!
+	* \param index : The index of the pair to remove
+	*/
 	void removePair(int index);
 
+	//! A function that tells if an Edge exist
+	/*!
+	* \param length : The length travelled
+	*/
 	void activityController(int length);
 private:
 	std::vector<std::pair<std::shared_ptr<AbstractNode>, int>> nodes_;

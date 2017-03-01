d4d64eec8d5a090f179e8912a27e206d6d529cca
locora <louis.cormier@polymtl.ca>
2017-03-01 16:49:56 -0500

    Ajout des entête aux fonctions de pathsearcher

--- tp1/Referentiel_pokemon_go/Referentiel_pokemon_go/PathSearcher.h
+++ tp1/Referentiel_pokemon_go/Referentiel_pokemon_go/PathSearcher.h
@@ -13,8 +13,22 @@ public:
 	~PathSearcher();
 #pragma endregion ConstructorDestructor
 
+	//! A function that prints the shortest path given a gain that we want
+	/*!
+	* \param theGraph_ : the graph on which we calculate the biggest gain
+	* \param startNode : The starting node
+	* \param gainWanted : The gain that we want
+	* \result : A string the path with the shortest path
+	*/
 	static std::string PathSearcher::ObtainShortestPath(std::shared_ptr<Graph> theGraph_, std::shared_ptr<AbstractNode> startNode, unsigned int gainWanted);
 
+	//! A function that prints the shortest without using Disjktra
+	/*!
+	* \param theGraph_ : the graph on which we calculate the biggest gain
+	* \param startKeyNode : The name of the starting node
+	* \param gainWanted : The gain that we want
+	* \result : A string the path with the shortest path
+	*/
 	static std::string ObtainShortestPathWithoutDisjktra(std::shared_ptr<Graph>  theGraph_, std::string startKeyNode, unsigned int gainWanted);
 
 	//! A function that prints the maximum gain given the maximum length

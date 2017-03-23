29cbfef2b63d9a3b6d7917eb707f0481d452d933
Adam Martin-Côté <adammartincote@gmail.com>
2017-03-23 16:24:22 -0400

    interface de NoeuFFF fait ensemble

--- tp2/AutomatesEtLanguages/AutomatesEtLanguages/Noeud.cpp
+++ tp2/AutomatesEtLanguages/AutomatesEtLanguages/Noeud.cpp
@@ -7,6 +7,43 @@ Noeud::Noeud()
 }
 
 
+Noeud::Noeud(std::string valeur, bool estUnMot, unsigned int niveau, Noeud parent)
+{
+}
+
+Noeud::Noeud(std::string valeur, bool estUnMot, unsigned int niveau)
+{
+}
+
 Noeud::~Noeud()
+{
+}
+
+std::vector<std::shared_ptr<Noeud>> Noeud::obtenirEnfants() const
+{
+	return std::vector<std::shared_ptr<Noeud>>();
+}
+
+Noeud Noeud::obtenirParent() const
+{
+	return Noeud();
+}
+
+bool Noeud::estUnMot() const
+{
+	return false;
+}
+
+unsigned int Noeud::obtenirNiveau() const
+{
+	return 0;
+}
+
+std::string Noeud::obtenirValeur() const
+{
+	return std::string();
+}
+
+void Noeud::ajouterEnfant(Noeud)
 {
 }
--- tp2/AutomatesEtLanguages/AutomatesEtLanguages/Noeud.h
+++ tp2/AutomatesEtLanguages/AutomatesEtLanguages/Noeud.h
@@ -1,8 +1,35 @@
+#include <string>
+#include <memory>
+#include <vector>
+
 #pragma once
 class Noeud
 {
 public:
-	Noeud();
+	// Constructeur racine
+	Noeud(std::string valeur, bool estUnMot, unsigned int niveau);
+
+	// Constructeur noeu[df]
+	Noeud(std::string valeur, bool estUnMot, unsigned int niveau, Noeud parent);
 	~Noeud();
+
+	std::vector<std::shared_ptr<Noeud>> obtenirEnfants() const;
+
+	Noeud obtenirParent() const;
+
+	bool estUnMot() const;		 
+												 
+	unsigned int obtenirNiveau() const;
+	std::string obtenirValeur() const;
+	
+	void ajouterEnfant(Noeud);
+
+private:
+	std::string valeur_;
+	bool estUnMot_;
+	unsigned int niveau_;
+
+	std::shared_ptr<Noeud> parent_;
+	std::vector<std::shared_ptr<Noeud>> enfants_;
 };
 

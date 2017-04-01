b1060313172a822f5a98e424175a15053d0ed349
Louis Cormier <locora@gigl.polymtl.ca>
2017-04-01 16:05:37 -0400

    Ajout fonction suggestions

--- tp2/AutomatesEtLanguages/AutomatesEtLanguages/GestionnaireSuggestions.cpp
+++ tp2/AutomatesEtLanguages/AutomatesEtLanguages/GestionnaireSuggestions.cpp
@@ -1,6 +1,7 @@
 #include "GestionnaireSuggestions.h"
 
 #define PREMIERE_VALEUR 0
+#define NOMBRE_DE_SUGGESTIONS 10
 
 GestionnaireSuggestions::GestionnaireSuggestions()
 {
@@ -27,15 +28,45 @@ std::shared_ptr<Noeud> GestionnaireSuggestions::RechercheNoeudMot(std::string mo
 		return nullptr;
 }
 
+std::vector<std::string> GestionnaireSuggestions::RemplirTableauSuggestions(std::vector<std::string>& suggestions, 
+	std::shared_ptr<Noeud> noeudDepart) {
+	GestionnaireSuggestions gestionnaireSuggestions;
+	if (suggestions.size() == NOMBRE_DE_SUGGESTIONS)
+		return suggestions;
+	else {
+		if (noeudDepart->estUnMot()) {
+			suggestions.push_back(noeudDepart->obtenirValeur());
+		}
+		for (auto noeud : noeudDepart->obtenirEnfants()) {
+			suggestions = gestionnaireSuggestions.RemplirTableauSuggestions(suggestions, noeud);
+		}
+	}
+	return suggestions;
+}
+
+
 void GestionnaireSuggestions::SuggestionsMots(std::shared_ptr<Lexique> lexique, std::string motEntree)const {
 	GestionnaireSuggestions gestionnaireSuggestion;
 	unsigned int compteur = 0;
 	std::shared_ptr<Noeud> temp;
-	for (auto noeud : lexique->ObtenirLettresArbres()) {
+	for (std::shared_ptr<Noeud> noeud : lexique->ObtenirLettresArbres()) {
 		if (noeud->obtenirValeur()[PREMIERE_VALEUR] == motEntree[PREMIERE_VALEUR])
 			temp = noeud;
 	}
 	temp = gestionnaireSuggestion.RechercheNoeudMot(motEntree, temp);
 	if (temp == nullptr)
 		std::cout << "Aucunes suggestions" << std::endl;
+	else {
+		std::cout << "Trouvé" << std::endl;
+		std::vector<std::string> suggestions;
+		suggestions = gestionnaireSuggestion.RemplirTableauSuggestions(suggestions, temp);
+		std::cout << suggestions;
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<std::string> suggestions) {
+	for (std::string suggestion : suggestions) {
+		os << suggestion << std::endl;
+	}
+	return os;
 }
--- tp2/AutomatesEtLanguages/AutomatesEtLanguages/GestionnaireSuggestions.h
+++ tp2/AutomatesEtLanguages/AutomatesEtLanguages/GestionnaireSuggestions.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 #include "Lexique.h"
 
 class GestionnaireSuggestions
@@ -12,6 +13,9 @@ public:
 
 	std::shared_ptr<Noeud> RechercheNoeudMot(std::string motEntree, 
 		std::shared_ptr<Noeud> noeudDeRecherche);
+	std::vector<std::string> RemplirTableauSuggestions(std::vector<std::string>& suggestions, std::shared_ptr<Noeud> noeudDepart);
 	void SuggestionsMots(std::shared_ptr<Lexique> lexique, std::string motEntree) const;
+
+	friend std::ostream& operator<<(std::ostream& os, const std::vector<std::string> suggestions);
 };
 
